e4faeb2c65bb701db37028d0d5a83ff4f3271682
wuchengxiao <39943244+wuchengxiao@users.noreply.github.com>
2026-06-17 22:41:03 +0800

    Create tongji.js

---
+++ tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1f19675b75b81e816d388c62922b180f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
